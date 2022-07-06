8b5a1fff0b59dadbda30f0bbe7011aeebca3d8de
THUNDER-PC\subha <rntvtf@pte.hu>
2022-07-06 18:51:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const images = ['https://picsum.photos/400', 'https://picsum.photos/800/350'];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="first-section">
+        <img className='responsive' src={images[0]} alt="random img" />
+        <div className='text'>
+          <p>CTA Title</p>
+          <button className='button'>CTA Button</button>
+        </div>
+        <img className='responsive' src={images[0]} alt="random img" />
+      </div>
+
+      <div className="second-section">
+        <div className='text2'>
+          <p>CTA Title</p>
+          <button className='button2'>CTA Button</button>
+        </div>
+        <img className='responsive' src={images[0]} alt="random img" />
+        <img className='responsive' src={images[0]} alt="random img" />
+        <img className='responsive' src={images[0]} alt="random img" />
+        <img className='responsive' src={images[0]} alt="random img" />
+        <img className='responsive' src={images[0]} alt="random img" />
+      </div>
+
+      <div className="third-section">
+        <img className='image3 responsive' src={images[1]} alt="random img" />
+        <div className='text3'>
+          <p>CTA Title</p>
+          <button className='button3'>CTA Button</button>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;

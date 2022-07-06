17309122fd22613e4f46cde014df6c7684376dd2
THUNDER-PC\subha <rntvtf@pte.hu>
2022-07-06 19:57:57 +0200

    carousal added...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function App() {
   const images = ['https://picsum.photos/400', 'https://picsum.photos/800/350'];
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  let carousalProp = true;
+
+  const carousal = (
+    <div>
+        <Slider {...settings}>
+        <div>
+          <img className='responsive' src={images[0]} alt="random img" />
+        </div>
+        <div>
+          <img className='responsive' src={images[0]} alt="random img" />
+        </div>
+        <div>
+          <img className='responsive' src={images[0]} alt="random img" />
+        </div>
+        <div>
+          <img className='responsive' src={images[0]} alt="random img" />
+        </div>
+        <div>
+          <img className='responsive' src={images[0]} alt="random img" />
+        </div>
+        <div>
+          <img className='responsive' src={images[0]} alt="random img" />
+        </div>
+      </Slider>
+      <div className='small-images'>
+          <img className='responsive' src={images[0]} alt="random img" />
+          <img className='responsive' src={images[0]} alt="random img" />
+          <img className='responsive' src={images[0]} alt="random img" />
+          <img className='responsive' src={images[0]} alt="random img" />
+          <img className='responsive' src={images[0]} alt="random img" />
+      </div>
+    </div>
+  )
 
   return (
     <div className="App">
@@ -33,6 +76,8 @@ function App() {
           <button className='button3'>CTA Button</button>
         </div>
       </div>
+
+      {carousalProp && carousal}
     </div>
   );
 }

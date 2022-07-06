9f53cfb7a325c3dc67eb1b7ed139d4b50c7d891d
THUNDER-PC\subha <rntvtf@pte.hu>
2022-07-06 20:50:54 +0200

    idk

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { useEffect, useState } from 'react';
 
 function App() {
   const images = ['https://picsum.photos/400', 'https://picsum.photos/800/350'];
@@ -12,10 +13,44 @@ function App() {
     slidesToShow: 1,
     slidesToScroll: 1
   };
-  let carousalProp = true;
+  const [carouselProp, setCarouselProp] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
-  const carousal = (
+  useEffect(() => {
+    function watchWidth() {
+      setWindowWidth(window.innerWidth)
+      if(windowWidth < 680) {
+        setCarouselProp(true);
+      } else {
+        setCarouselProp(false);
+      }
+    }
+    window.addEventListener('resize', watchWidth);
+    return function() {
+      window.removeEventListener('resize', watchWidth);
+    }
+  }, [windowWidth])
+
+  const desktopGrid = (
+    <div className="second-section">
+        <div className='text2'>
+          <p>CTA Title</p>
+          <button className='button2'>CTA Button</button>
+        </div>
+        <img className='responsive' src={images[0]} alt="random img" />
+        <img className='responsive' src={images[0]} alt="random img" />
+        <img className='responsive' src={images[0]} alt="random img" />
+        <img className='responsive' src={images[0]} alt="random img" />
+        <img className='responsive' src={images[0]} alt="random img" />
+      </div>
+  )
+
+  const carousel = (
     <div>
+        <div className='text2'>
+          <p>CTA Title</p>
+          <button className='button2'>CTA Button</button>
+        </div>
         <Slider {...settings}>
         <div>
           <img className='responsive' src={images[0]} alt="random img" />
@@ -57,18 +92,8 @@ function App() {
         <img className='responsive' src={images[0]} alt="random img" />
       </div>
 
-      <div className="second-section">
-        <div className='text2'>
-          <p>CTA Title</p>
-          <button className='button2'>CTA Button</button>
-        </div>
-        <img className='responsive' src={images[0]} alt="random img" />
-        <img className='responsive' src={images[0]} alt="random img" />
-        <img className='responsive' src={images[0]} alt="random img" />
-        <img className='responsive' src={images[0]} alt="random img" />
-        <img className='responsive' src={images[0]} alt="random img" />
-      </div>
-
+      {carouselProp ? carousel : desktopGrid}
+      
       <div className="third-section">
         <img className='image3 responsive' src={images[1]} alt="random img" />
         <div className='text3'>
@@ -76,11 +101,8 @@ function App() {
           <button className='button3'>CTA Button</button>
         </div>
       </div>
-
-      {carousalProp && carousal}
     </div>
   );
 }
-
 
 export default App;
